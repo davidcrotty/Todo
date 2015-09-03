015db4a1cfeec1d90e098fc89c9d892eb976954c
David Cotty <david.crotty@vualto.com>
2015-09-03 18:10:47 +0100

    Added dagger DI

---
+++ app/src/main/java/com/vualto/todo/presenter/TodoListPresenter.java
@@ -0,0 +1,15 @@
+package com.vualto.todo.presenter;
+
+import com.vualto.todo.TodoListActivity;
+
+/**
+ * Created by David on 03/09/2015.
+ */
+public class TodoListPresenter {
+
+    public TodoListActivity _todolistActivity;
+
+    public TodoListPresenter(TodoListActivity todolistActivity) {
+        _todolistActivity = todolistActivity;
+    }
+}

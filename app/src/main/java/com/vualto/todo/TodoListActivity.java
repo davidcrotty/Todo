ae15a9a1d3069c09dd9d5436344d93d18252715c
David Cotty <david.crotty@vualto.com>
2015-09-03 18:00:19 +0100

    Refactored main activity name.

--- app/src/main/java/com/vualto/todo/TodoListActivity.java
+++ app/src/main/java/com/vualto/todo/TodoListActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+public class TodoListActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

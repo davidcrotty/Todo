bf32347d918a566cd5494cd04e9bf0e38ddd87ec
David Cotty <david.crotty@vualto.com>
2015-09-06 20:36:20 +0100

    Updated Activity hierarchy.

---
+++ app/src/main/java/com/vualto/todo/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.vualto.todo;
+
+import android.app.Activity;
+
+/**
+ * Created by David on 06/09/2015.
+ */
+public class BaseActivity extends Activity {
+}
--- app/src/main/java/com/vualto/todo/TodoListActivity.java
+++ app/src/main/java/com/vualto/todo/TodoListActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class TodoListActivity extends Activity {
+public class TodoListActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

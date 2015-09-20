2d6d6aae09123b8d3d63ceba76c4bc90fa929f24
David Cotty <david.crotty@vualto.com>
2015-09-20 14:48:54 +0100

    Removed fragment use, overcomplicates app flow / navigation.

--- app/src/main/java/com/vualto/todo/TodoListActivity.java
+++ app/src/main/java/com/vualto/todo/TodoListActivity.java
@@ -1,6 +1,11 @@
 package com.vualto.todo;
 
+import android.animation.Animator;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.AccelerateInterpolator;
 
 import com.vualto.todo.module.DaggerDataRepositoryComponent;
 import com.vualto.todo.module.DaggerTodoListPresenterComponent;
@@ -12,19 +17,27 @@ import com.vualto.todo.presenter.TodoListPresenter;
 
 import javax.inject.Inject;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import dagger.Lazy;
 
-public class TodoListActivity extends BaseActivity  {
+public class TodoListActivity extends BaseActivity implements View.OnLayoutChangeListener,
+                                                              Animator.AnimatorListener {
 
     private DataRepositoryComponent _dataRepositoryComponent;
     private TodoListPresenterComponent _component;
     @Inject Lazy<TodoListPresenter> _presenter;
 
+    @Bind(R.id.add_item_button)
+    FloatingActionButton _addItemButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        _addItemButton.addOnLayoutChangeListener(this);
          _dataRepositoryComponent = DaggerDataRepositoryComponent.builder()
                                    .dataRepositoryModule(new DataRepositoryModule()).build();
 
@@ -32,10 +45,51 @@ public class TodoListActivity extends BaseActivity  {
         .dataRepositoryComponent(_dataRepositoryComponent)
         .todoListPresenterModule(new TodoListPresenterModule(this))
         .build().inject(this);
-        _presenter.get().launchTodoListFragment(this);
     }
 
-    public TodoListPresenter getPresenter() {
-        return _presenter.get();
+    @OnClick(R.id.add_item_button)
+    void onClick() {
+        int startRadius = Math.max(_addItemButton.getWidth(), _addItemButton.getHeight());
+        int cx = _addItemButton.getWidth() / 2;
+        int cy = _addItemButton.getHeight() / 2;
+
+        Animator anim = ViewAnimationUtils.createCircularReveal(_addItemButton, cx, cy, startRadius, 0);
+        anim.addListener(this);
+        anim.setInterpolator(new AccelerateInterpolator());
+        anim.setDuration(700);
+        anim.start();
+    }
+
+    @Override
+    public void onAnimationStart(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animation) {
+        _addItemButton.setVisibility(View.INVISIBLE);
+
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animation) {
+
+    }
+
+    @Override
+    public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+        int finalRadius = Math.max(_addItemButton.getWidth(), _addItemButton.getHeight());
+        int cx = _addItemButton.getWidth() / 2;
+        int cy = _addItemButton.getHeight() / 2;
+
+        Animator anim = ViewAnimationUtils.createCircularReveal(_addItemButton, cx, cy, 0, finalRadius);
+        anim.setInterpolator(new AccelerateInterpolator());
+        anim.setDuration(700);
+        anim.start();
     }
 }
--- app/src/main/java/com/vualto/todo/presenter/TodoListPresenter.java
+++ app/src/main/java/com/vualto/todo/presenter/TodoListPresenter.java
@@ -1,18 +1,7 @@
 package com.vualto.todo.presenter;
 
-import android.app.Activity;
-import android.content.Context;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
-
-import com.vualto.todo.R;
 import com.vualto.todo.TodoListActivity;
-import com.vualto.todo.fragment.TodoListFragment;
 import com.vualto.todo.service.TaskService;
-
-import javax.inject.Inject;
 
 /**
  * Created by David on 03/09/2015.
@@ -25,16 +14,5 @@ public class TodoListPresenter {
     public TodoListPresenter(TodoListActivity todolistActivity, TaskService taskService) {
         _todolistActivity = todolistActivity;
         _taskService = taskService;
-    }
-
-    public void launchTodoListFragment(FragmentActivity activity) {
-        FragmentTransaction ft = activity.getSupportFragmentManager().beginTransaction();
-        Fragment prev = activity.getSupportFragmentManager().findFragmentByTag(TodoListFragment.class.getName());
-        if(prev != null) {
-            ft.remove(prev);
-        }
-        ft.addToBackStack(null);
-        ft.add(R.id.todolist_fragment_container, new TodoListFragment(), TodoListFragment.class.getName());
-        ft.commit();
     }
 }

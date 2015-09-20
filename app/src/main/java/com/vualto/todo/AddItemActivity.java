8f2b355d0c60f51ce2998d47b97b0e320204b262
David Cotty <david.crotty@vualto.com>
2015-09-20 15:40:23 +0100

    Hide keyboard on start of activity. Use coordinator layout so snackbar can be used.

--- app/src/main/java/com/vualto/todo/AddItemActivity.java
+++ app/src/main/java/com/vualto/todo/AddItemActivity.java
@@ -1,14 +1,36 @@
 package com.vualto.todo;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by David on 20/09/2015.
  */
 public class AddItemActivity extends BaseActivity {
+
+    @Bind(R.id.notes_edit_text)
+    EditText _notesText;
+
+    @Bind(R.id.save_item_button)
+    FloatingActionButton _saveItemButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        ButterKnife.bind(this);
+
+    }
+
+    @OnClick(R.id.save_item_button)
+    void onClick() {
+        
     }
 }

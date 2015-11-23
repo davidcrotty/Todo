d473454696cbb25442cf4fbcace9461810ffa534
David\David Crotty <david.crotty.work@gmail.com>
2015-11-23 23:27:51 +0000

    Update margins. No todo items become scrollable with lack of screen real estate. Keeps icon size consistency.

--- app/src/main/java/com/david/todo/view/NoTodoItemsView.java
+++ app/src/main/java/com/david/todo/view/NoTodoItemsView.java
@@ -1,6 +1,7 @@
 package com.david.todo.view;
 
 import android.content.Context;
+import android.util.AttributeSet;
 import android.widget.LinearLayout;
 
 import com.david.todo.R;
@@ -12,6 +13,15 @@ public class NoTodoItemsView extends LinearLayout {
 
     public NoTodoItemsView(Context context) {
         super(context);
+        init();
+    }
+
+    public NoTodoItemsView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
         inflate(getContext(), R.layout.no_todo_items_view, this);
     }
 }

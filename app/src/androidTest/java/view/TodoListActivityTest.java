cb6ddb216a973b0a5c1e26bd78d6206c500a4ae2
David Crotty <david.crotty.work@gmail.com>
2016-01-02 19:59:51 +0000

    Add test to ensure note options disappear when entering text.

--- app/src/androidTest/java/view/TodoListActivityTest.java
+++ app/src/androidTest/java/view/TodoListActivityTest.java
@@ -1,5 +1,6 @@
 package view;
 
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -14,8 +15,9 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class TodoListActivityTest {
@@ -26,10 +28,15 @@ public class TodoListActivityTest {
 
     @Test
     public void when_inputting_short_text() {
-        onView(withId(R.id.short_note_text)).perform(typeText("NewText"),
+        System.out.println("Given user TodoListActivity");
+
+        System.out.println("When inputting text");
+        onView(withId(R.id.short_note_text)).perform(typeText("Input"),
                 closeSoftKeyboard());
 
-        onView(withId(R.id.short_note_text)).check(matches(withText("NewText")));
+        System.out.println("Options panel Should disappear");
+        onView(withId(R.id.options_panel)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.INVISIBLE)));
+        onView(withId(R.id.options_divider)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.INVISIBLE)));
     }
 
 

5e5b44f16648b650a6eb42b1758008a908cf7af1
David Crotty <david.crotty.work@gmail.com>
2016-04-10 01:26:54 +0100

    Sets tab colour

--- app/src/main/java/com/david/todo/view/widgets/TabView.kt
+++ app/src/main/java/com/david/todo/view/widgets/TabView.kt
@@ -1,11 +1,10 @@
 package com.david.todo.view.widgets
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import com.david.todo.R
 
 /**
  * Created by DavidHome on 07/04/2016.
@@ -13,19 +12,35 @@ import android.view.View
 class TabView(context: Context, attributes: AttributeSet) : View(context, attributes) {
 
     lateinit var paint: Paint
+    var color: Int
 
     init {
         paint = Paint()
+        color = context.resources.getColor(R.color.red)
     }
 
     override fun onDraw(canvas: Canvas?) {
-        paint.setColor(Color.RED);
-        paint.setStrokeWidth(3F);
-        canvas?.drawRect(30F, 30F, 80F, 80F, paint);
-        paint.setStrokeWidth(0F);
-        paint.setColor(Color.CYAN);
-        canvas?.drawRect(33F, 60F, 77F, 77F, paint );
-        paint.setColor(Color.YELLOW);
-        canvas?.drawRect(33F, 33F, 77F, 60F, paint );
+        val canvas = canvas?.let { it } ?: return
+        paint.color = color;
+        paint.strokeWidth = 3F;
+        canvas.drawRect(40F,0F, 150F, 40F, paint)
+
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        paint.setAntiAlias(true)
+        val path = Path();
+        path.setFillType(Path.FillType.EVEN_ODD);
+        path.lineTo(40F, 40F);
+        path.lineTo(40F, 0F);
+        path.lineTo(0F, 0F);
+        path.close();
+
+        canvas.drawPath(path, paint);
+
+//        canvas?.drawRect(30F, 30F, 80F, 80F, paint);
+//        paint.setStrokeWidth(0F);
+//        paint.setColor(Color.CYAN);
+//        canvas?.drawRect(33F, 60F, 77F, 77F, paint );
+//        paint.setColor(Color.YELLOW);
+//        canvas?.drawRect(33F, 33F, 77F, 60F, paint );
     }
 }

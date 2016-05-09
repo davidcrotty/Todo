f10358b897ccaa173d40d4fb0c55a7bdda367da8
David Crotty <david.crotty.work@gmail.com>
2016-05-09 21:40:00 +0100

    Basic shaping for speech widget.

--- app/src/main/java/com/david/todo/view/widgets/SpeechView.kt
+++ app/src/main/java/com/david/todo/view/widgets/SpeechView.kt
@@ -1,9 +1,9 @@
 package com.david.todo.view.widgets
 
 import android.content.Context
-import android.content.res.TypedArray
 import android.graphics.Canvas
-import android.graphics.drawable.Drawable
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 import com.david.todo.R
@@ -13,13 +13,31 @@ import com.david.todo.R
  */
 class SpeechView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
+    lateinit var triangle: Path
+    lateinit var fill: Paint
+
     init {
+        triangle = Path()
+        triangle.fillType = Path.FillType.EVEN_ODD
+        fill = Paint()
+        fill.color = context.resources.getColor(android.R.color.white);
+        fill.strokeWidth = 1F;
+        fill.style = Paint.Style.FILL_AND_STROKE
+        fill.isAntiAlias = true
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        val d = resources.getDrawable(R.drawable.speech_container);
-        d.setBounds(0, 0, canvas!!.width, canvas!!.height);
-        d.draw(canvas);
+        val speechBubble = resources.getDrawable(R.drawable.speech_container);
+        speechBubble.setBounds(0, 0, canvas!!.width - 48, canvas!!.height);
+        speechBubble.draw(canvas);
+
+        triangle.moveTo(canvas!!.width.toFloat(), 0F)
+        triangle.lineTo(canvas!!.width.toFloat(), 0F)
+        triangle.lineTo(canvas!!.width.toFloat() - 48F, 0F)
+        triangle.lineTo(canvas!!.width.toFloat() - 48F, 50F)
+        triangle.close()
+
+        canvas.drawPath(triangle, fill)
     }
 }

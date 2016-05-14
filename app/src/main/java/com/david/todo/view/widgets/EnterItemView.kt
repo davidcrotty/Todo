29d90f7de6fa773276a2cbbe89025395ec8ff0ac
David Crotty <david.crotty.work@gmail.com>
2016-05-14 21:44:34 +0100

    Activates/deactivates add button depending on text entered.

--- app/src/main/java/com/david/todo/view/widgets/EnterItemView.kt
+++ app/src/main/java/com/david/todo/view/widgets/EnterItemView.kt
@@ -2,18 +2,23 @@ package com.david.todo.view.widgets
 
 import android.content.Context
 import android.content.res.ColorStateList
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.AttributeSet
+import android.view.View
+import android.widget.EditText
 import android.widget.FrameLayout
 import android.widget.ImageView
 import butterknife.bindView
 import com.david.todo.R
+import timber.log.Timber
 
 /**
  * Created by DavidHome on 08/05/2016.
  */
-class EnterItemView(context: Context, attrs: AttributeSet) : FrameLayout(context, attrs) {
-
+class EnterItemView(context: Context, attrs: AttributeSet) : FrameLayout(context, attrs), View.OnClickListener, TextWatcher {
     val sendImage: ImageView by bindView(R.id.send_button)
+    val editText: EditText by bindView(R.id.add_item_text_edit)
     val iconColour: ColorStateList
 
     init {
@@ -21,6 +26,22 @@ class EnterItemView(context: Context, attrs: AttributeSet) : FrameLayout(context
         val typedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.speech_view, 0, 0);
         iconColour = typedArray.getColorStateList(R.styleable.speech_view_icon_colour)
         typedArray.recycle();
+        editText.addTextChangedListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        
+    }
+
+    override fun afterTextChanged(editable: Editable?) {
+        val enteredText = editable.toString()
+        sendImage.setOnClickListener(if (enteredText == null || enteredText.isBlank()) null else this)
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
     }
 
     override fun onFinishInflate() {

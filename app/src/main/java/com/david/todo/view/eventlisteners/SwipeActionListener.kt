907b97e16d2e8593664bf62f3239d114b99e2034
David Crotty <david.crotty.work@gmail.com>
2016-06-09 18:34:35 +0100

    Separates out identification and selection of view to swipe.

--- app/src/main/java/com/david/todo/view/eventlisteners/SwipeActionListener.kt
+++ app/src/main/java/com/david/todo/view/eventlisteners/SwipeActionListener.kt
@@ -3,6 +3,7 @@ package com.david.todo.view.eventlisteners
 import android.support.v7.widget.RecyclerView
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewGroup
 import com.david.todo.adapter.viewholder.PendingItemViewHolder
 import timber.log.Timber
 
@@ -12,7 +13,8 @@ import timber.log.Timber
 class SwipeActionListener : RecyclerView.OnItemTouchListener {
 
     val NO_TRANSLATION: Float = 0F
-    var selectedViewForeground: View? = null
+    var selectedViewForeground: ViewGroup? = null
+
 
     override fun onTouchEvent(rv: RecyclerView?, e: MotionEvent?) {
         Timber.d("onTouchEvent")
@@ -26,10 +28,7 @@ class SwipeActionListener : RecyclerView.OnItemTouchListener {
                     return false
                 }
 
-                var view = rv?.findChildViewUnder(e!!.x, e.y)
-                if(view != null) {
-                    findAndSelectViewIfValidViewHolderItem(view, rv)
-                }
+                findAndSelectViewIfValidViewHolderItem(rv?.findChildViewUnder(e!!.x, e.y), rv)
             }
             MotionEvent.ACTION_UP -> {
                 if(selectedViewForeground != null) { //ping view back to original position TODO check here no other animation was triggered
@@ -43,17 +42,30 @@ class SwipeActionListener : RecyclerView.OnItemTouchListener {
         return false
     }
 
-    fun findAndSelectViewIfValidViewHolderItem(view: View, recyclerView: RecyclerView?) {
+    /**
+     * Finds out if view is part of this rv's list and a valid item type, as only Pending
+     * items are allowed to be the swiped
+     */
+    fun findAndSelectViewIfValidViewHolderItem(view: View?, recyclerView: RecyclerView?) {
+        val view = view?.let { it } ?: return
+
         if(selectedViewForeground == null) { //First time at selecting a view?
             var viewHolder = recyclerView?.getChildViewHolder(view) //get rv's view holder
             if(viewHolder is PendingItemViewHolder) { //are we the right type of viewholder? TODO (As a lib parent view could be of certain type
                 var pendingItemHolder = viewHolder
-                selectedViewForeground = pendingItemHolder.taskForeground
+                selectViewHolder(pendingItemHolder.taskForeground)
                 Timber.d("View selected, translation ${selectedViewForeground?.translationX}")
             } else {
-                selectedViewForeground = null
+                selectViewHolder(null)
             }
         }
+    }
+
+    /**
+     * Selects viewholder, records it as a local variable as well as associates a delta value with it
+     */
+    fun selectViewHolder(view: ViewGroup?) {
+        selectedViewForeground = view
     }
 
 

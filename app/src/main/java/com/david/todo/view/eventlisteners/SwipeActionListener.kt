bfeea8e18e7dd739cb3df2c8cf1e8a6bfa42210d
David Crotty <david.crotty.work@gmail.com>
2016-06-10 17:44:55 +0100

    Allows dragging of foreground view.

--- app/src/main/java/com/david/todo/view/eventlisteners/SwipeActionListener.kt
+++ app/src/main/java/com/david/todo/view/eventlisteners/SwipeActionListener.kt
@@ -15,27 +15,26 @@ class SwipeActionListener : RecyclerView.OnItemTouchListener {
     val NO_TRANSLATION: Float = 0F
     var selectedViewForeground: ViewGroup? = null
 
+    //Translation item TODO could become a value object
+    var deltaMoveX:Float = 0F
 
     override fun onTouchEvent(rv: RecyclerView?, e: MotionEvent?) {
         Timber.d("onTouchEvent")
     }
 
-    override fun onInterceptTouchEvent(rv: RecyclerView?, e: MotionEvent?): Boolean {
-        when(e?.action) {
+    override fun onInterceptTouchEvent(rv: RecyclerView?, event: MotionEvent?): Boolean {
+        when(event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                findAndSelectViewIfValidViewHolderItem(rv?.findChildViewUnder(event!!.x, event.y), rv, event!!)
+            }
             MotionEvent.ACTION_MOVE -> {
                 if(selectedViewForeground != null) {
-                    selectedViewForeground?.translationX = e?.rawX!!
+                    selectedViewForeground?.translationX = deltaMoveX + event?.rawX!!
                     return false
                 }
-
-                findAndSelectViewIfValidViewHolderItem(rv?.findChildViewUnder(e!!.x, e.y), rv)
             }
             MotionEvent.ACTION_UP -> {
-                if(selectedViewForeground != null) { //ping view back to original position TODO check here no other animation was triggered
-                    selectedViewForeground?.translationX = NO_TRANSLATION
-                    Timber.d("Released view")
-                }
-                selectedViewForeground = null
+                deSelectViewHolder()
             }
         }
 
@@ -46,17 +45,17 @@ class SwipeActionListener : RecyclerView.OnItemTouchListener {
      * Finds out if view is part of this rv's list and a valid item type, as only Pending
      * items are allowed to be the swiped
      */
-    fun findAndSelectViewIfValidViewHolderItem(view: View?, recyclerView: RecyclerView?) {
+    fun findAndSelectViewIfValidViewHolderItem(view: View?, recyclerView: RecyclerView?, motionEvent: MotionEvent) {
         val view = view?.let { it } ?: return
 
         if(selectedViewForeground == null) { //First time at selecting a view?
             var viewHolder = recyclerView?.getChildViewHolder(view) //get rv's view holder
             if(viewHolder is PendingItemViewHolder) { //are we the right type of viewholder? TODO (As a lib parent view could be of certain type
                 var pendingItemHolder = viewHolder
-                selectViewHolder(pendingItemHolder.taskForeground)
+                selectViewHolder(pendingItemHolder.taskForeground, motionEvent)
                 Timber.d("View selected, translation ${selectedViewForeground?.translationX}")
             } else {
-                selectViewHolder(null)
+                selectViewHolder(null, motionEvent)
             }
         }
     }
@@ -64,8 +63,20 @@ class SwipeActionListener : RecyclerView.OnItemTouchListener {
     /**
      * Selects viewholder, records it as a local variable as well as associates a delta value with it
      */
-    fun selectViewHolder(view: ViewGroup?) {
+    fun selectViewHolder(view: ViewGroup?, motionEvent: MotionEvent) {
         selectedViewForeground = view
+        if(view != null) {
+            deltaMoveX = selectedViewForeground!!.x - motionEvent.rawX
+        }
+
+    }
+
+    fun deSelectViewHolder() {
+        if(selectedViewForeground != null) { //ping view back to original position TODO check here no other animation was triggered
+            selectedViewForeground?.translationX = NO_TRANSLATION
+            deltaMoveX = NO_TRANSLATION
+        }
+        selectedViewForeground = null
     }
 
 

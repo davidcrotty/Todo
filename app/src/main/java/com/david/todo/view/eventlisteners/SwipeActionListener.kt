e633f25fcea41a9f7727f525764535a1247d1819
David Crotty <david.crotty.work@gmail.com>
2016-06-12 19:19:46 +0100

    Fling detection using Android SDK Velocity tracker

--- app/src/main/java/com/david/todo/view/eventlisteners/SwipeActionListener.kt
+++ app/src/main/java/com/david/todo/view/eventlisteners/SwipeActionListener.kt
@@ -2,60 +2,64 @@ package com.david.todo.view.eventlisteners
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
-import android.view.GestureDetector
-import android.view.MotionEvent
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import com.david.todo.adapter.viewholder.PendingItemViewHolder
 import timber.log.Timber
 
 /**
  * Created by DavidHome on 07/06/2016.
  */
-class SwipeActionListener() : RecyclerView.OnItemTouchListener {
+
+/**
+ * @param context - Used for getting view configuration in determining fling distance per device
+ */
+class SwipeActionListener(val context: Context) : RecyclerView.OnItemTouchListener {
 
     val NO_TRANSLATION: Float = 0F
     val SWIPE_OFF_SCALAR: Int = 2
 
     var selectedViewForeground: ViewGroup? = null
+    var shouldFlingRightOffScreen: Boolean = false
 
     //Translation item TODO could become a value object
     var deltaMoveX: Float = 0F
-    var shouldFlingRightOffScreen = false
+    var velocityTracker : VelocityTracker? = null
 
     override fun onTouchEvent(rv: RecyclerView?, e: MotionEvent?) {
         Timber.d("onTouchEvent")
     }
 
     override fun onInterceptTouchEvent(rv: RecyclerView?, event: MotionEvent?): Boolean {
-        //check if fling on action down
-        //if was fling swipe view left or right based on direction, gesture detector can call this
-        //class back?
-
-        //if fling detected call back this class
 
         when(event?.action) {
             MotionEvent.ACTION_DOWN -> {
                 findAndSelectViewIfValidViewHolderItem(rv?.findChildViewUnder(event!!.x, event.y), rv, event!!)
+                if(selectedViewForeground != null) {
+                    velocityTracker = VelocityTracker.obtain()
+                    velocityTracker?.addMovement(event)
+                }
             }
             MotionEvent.ACTION_MOVE -> {
                 translateViewIfOneSelected(event)
-                Timber.d("Delta $deltaMoveX")
-                if(event!!.x > -350) { //right direction, better off a percentage /
-                    var move = event!!.eventTime - event.downTime
-                    Timber.d("Move $move")
-                    if(event!!.eventTime - event.downTime < 85) {
-                        findAndSelectViewIfValidViewHolderItem(rv?.findChildViewUnder(event!!.x, event.y), rv, event!!)
-                        if(selectedViewForeground == null) return false
-                        SliderAnimationWrapper(selectedViewForeground!!,
-                                selectedViewForeground?.translationX!!,
-                                selectedViewForeground?.width!!.toFloat())
-                                .start()
-                        selectedViewForeground = null
-                    }
+                if(selectedViewForeground != null) {
+                    velocityTracker?.addMovement(event)
                 }
             }
             MotionEvent.ACTION_UP -> {
+                if(selectedViewForeground != null) {
+                    var viewConfiguration = ViewConfiguration.get(context)
+                    velocityTracker?.addMovement(event)
+                    velocityTracker?.computeCurrentVelocity(1000, viewConfiguration.scaledMaximumFlingVelocity.toFloat())
+                    var velocity = velocityTracker?.getXVelocity(event!!.getPointerId(event!!.actionIndex))
+                    //TODO will need to determine direction also
+                    Timber.d("Velocity ${velocity!!.toInt()}")
+                    if(Math.abs(velocity!!.toInt()) > 1200) {
+                        Timber.d("FLING DETECTED")
+                    }
+
+                    velocityTracker?.recycle()
+                }
+
                 if(shouldFlingRightOffScreen) {
                     if(selectedViewForeground != null) {
                         SliderAnimationWrapper(selectedViewForeground!!,
